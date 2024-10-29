4831643bfe407538e96741416003acceb46d1716
Petr Malanik <Malanik.Petr.gm@gmail.com>
2024-10-29 23:59:33 +0100

    messages: add messages performing bootloader entry and reset

---
+++ messages/device_reset.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file device_reset.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 29.10.2024
+ */
+
+#pragma once
+
+#include "codes/codes.hpp"
+
+#include "can_bus/app_message.hpp"
+#include "empty_data_message.hpp"
+
+namespace App_messages {
+struct Device_reset : Empty_data_message {
+    Device_reset() :
+        Base_message(Codes::Message_type::Device_reset)
+    { }
+};
+};
---
+++ messages/device_usb_bootloader.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file device_usb_bootloader.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 29.10.2024
+ */
+
+#pragma once
+
+#include "codes/codes.hpp"
+
+#include "can_bus/app_message.hpp"
+#include "empty_data_message.hpp"
+
+namespace App_messages {
+struct Device_usb_bootloader : Empty_data_message {
+    Device_usb_bootloader() :
+        Base_message(Codes::Message_type::Device_usb_bootloader)
+    { }
+};
+};
