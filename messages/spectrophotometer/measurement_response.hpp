02c0dcbfa60344e1350871ed9c02cd19b53ca15b
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-04-16 16:23:00 +0200

    spectrophotometer: fix absolute value conversion in measurement response

--- messages/spectrophotometer/measurement_response.hpp
+++ messages/spectrophotometer/measurement_response.hpp
@@ -25,14 +25,15 @@ namespace App_messages::Spectrophotometer {
         uint16_t absolute_value = 0;
 
 
-        explicit Measurement_response(uint8_t channel, float value):
+        explicit Measurement_response(uint8_t channel, float relative_value, float absolute_value):
             Base_message(Codes::Message_type::Spectrophotometer_measurement_response),
             channel(channel),
-            value(value)
+            relative_value(relative_value),
+            absolute_value(absolute_value)
         {}
 
         Measurement_response():
-            Measurement_response(0, 0)
+            Measurement_response(0, 0 , 0)
         {}
 
         virtual bool Interpret_data(can_data_vector_t &data) override final {
@@ -42,13 +43,15 @@ namespace App_messages::Spectrophotometer {
 
             channel = data[0];
             relative_value = *reinterpret_cast<float *>(&data[1]);
-            absolute_value = *reinterpret_cast<uint16_t *>(&data[5]);
+
+            absolute_value = (data[5] << 8);
+            absolute_value |= data[6] & 0xFF;
 
             return true;
         }
 
         virtual can_data_vector_t Export_data() override final {
-            can_data_vector_t data(5);
+            can_data_vector_t data(7);
             data[0] = channel;
 
             auto value_data = reinterpret_cast<uint8_t *>(&relative_value);
@@ -57,9 +60,8 @@ namespace App_messages::Spectrophotometer {
             data[3] = value_data[2];
             data[4] = value_data[3];
 
-            auto absolute_value_data = reinterpret_cast<uint8_t *>(&absolute_value);
-            data[5] = absolute_value_data[0];
-            data[6] = absolute_value_data[1];
+            data[5] = absolute_value >> 8;
+            data[6] = absolute_value & 0xFF;
 
             return data;
         }
